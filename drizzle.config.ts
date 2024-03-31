7ebd1312346139ca01c38e7fbd27975775dc22b6
hubertbodek <hubert.bodek1@gmail.com>
2024-03-31 12:51:45 +0200

    db setup, clerk setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,16 @@
 import type { Config } from 'drizzle-kit';
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
 export default {
 	schema: './src/db/schemas',
 	out: './migrations',
 	driver: 'turso',
 	dbCredentials: {
-		url: process.env.TURSO_CONNECTION_URL!,
+		url: process.env.TURSO_DATABASE_URL!,
 		authToken: process.env.TURSO_AUTH_TOKEN!,
 	},
 } satisfies Config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-export default function Home() {
-	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			elo
-		</main>
-	);
+import { db } from '@/db'
+import { posts as postsTable } from '@/db/schemas/posts'
+
+export default async function Home() {
+  const posts = await db.select().from(postsTable).orderBy(postsTable.createdAt).limit(10)
+
+  console.log({ posts })
+
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">elo</main>
 }
