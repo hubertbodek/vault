70486273a70b3d2fcbc05d14b242425797d4420e
hubertbodek <hubert.bodek1@gmail.com>
2024-03-31 12:04:14 +0200

    db setup, shadcn setup

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'drizzle-kit';
+export default {
+	schema: './src/db/schemas',
+	out: './migrations',
+	driver: 'turso',
+	dbCredentials: {
+		url: process.env.TURSO_CONNECTION_URL!,
+		authToken: process.env.TURSO_AUTH_TOKEN!,
+	},
+} satisfies Config;
